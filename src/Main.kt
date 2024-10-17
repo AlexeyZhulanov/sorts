bc256e708b9baa646a97ae9380d83089298165fe
alex <Leha.Zhulanov.03@mail.ru>
2024-10-17 15:46:38 +0500

    Task2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
-    println("Hello World!")
+    // Task 2
+    var a = 8
+    var b = 10
+
+    a = a + b // a = 18
+    b = a - b // b = 8
+    a = a - b // a = 10
+
+    println("a: $a, b: $b") // a: 10, b: 8
 }
