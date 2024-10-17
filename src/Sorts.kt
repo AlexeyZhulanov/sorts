aee4c82c94d1b7ed2b5f8801e569a4308f6e23fa
alex <Leha.Zhulanov.03@mail.ru>
2024-10-17 15:30:22 +0500

    first sort

---
+++ src/Sorts.kt
@@ -0,0 +1,15 @@
+class Sorts() {
+    fun insertionSort(arr: IntArray): IntArray {
+        for (i in 1..<arr.size) {
+            val key = arr[i]
+            var j = i - 1
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j]
+                j--
+            }
+            arr[j + 1] = key
+        }
+        return arr
+    }
+
+}
---
+++ src/Tests.kt
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class SortingTests {
+
+    private val sorts = Sorts()
+
+    @Test
+    fun testInsertionSort() {
+        val input = intArrayOf(5, 2, 9, 1, 5, 6)
+        val expected = intArrayOf(1, 2, 5, 5, 6, 9)
+        assertArrayEquals(expected, sorts.insertionSort(input))
+    }
+
+    @Test
+    fun testSortingWithNegativeNumbers() {
+        val input = intArrayOf(-3, -1, -4, 2, 0, 1)
+        val expected = intArrayOf(-4, -3, -1, 0, 1, 2)
+        assertArrayEquals(expected, sorts.insertionSort(input))
+    }
+
+    @Test
+    fun testSortingAlreadySortedArray() {
+        val input = intArrayOf(1, 2, 3, 4, 5)
+        assertArrayEquals(input, sorts.insertionSort(input.clone()))
+    }
+}
